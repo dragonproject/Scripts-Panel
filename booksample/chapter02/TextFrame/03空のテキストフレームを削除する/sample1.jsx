00d3a424d8a43c90690d086866d0d0d9b676ded5
関龍太郎 <sekiryutaro@MacBook-Pro.local>
2016-08-31 23:05:11 +0900

    とにかくコミット

--- booksample/chapter02/TextFrame/03空のテキストフレームを削除する/sample1.jsx
+++ booksample/chapter02/TextFrame/03空のテキストフレームを削除する/sample1.jsx
@@ -1,1 +1,15 @@
-(function(){	var docObj = app.activeDocument;	for (var i=0; i<docObj.pages.length; i++)	{		var tfObj = docObj.pages[i].textFrames;		for (var j=tfObj.length-1; j>=0; j--)		{			if (tfObj[j].contents == "")			{				tfObj[j].remove();			}		}	}})();
+(function(){
+	var docObj = app.activeDocument;
+	for (var i=0; i<docObj.pages.length; i++)
+	{
+		var tfObj = docObj.pages[i].textFrames;
+		for (var j=tfObj.length-1; j>=0; j--)
+		{
+			if (tfObj[j].contents == "")
+			{
+				tfObj[j].remove();
+//				tfObj[j].fillColor(0,100,100,0);
+			}
+		}
+	}
+})();
